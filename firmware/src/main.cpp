c39728d1bf81c5a87578377add8354989cdeea57
Dylan Jiao <dylanjiao2001@gmail.com>
2025-12-22 16:23:24 -0500

    feat: add I2C scanner

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,42 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <Wire.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(500);
+
+  // I2C defaults for ESP32 are usually SDA=21, SCL=22
+  Wire.begin(21, 22);
+
+  Serial.println("\nI2C Scanner");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  byte error, address;
+  int found = 0;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("Scanning...");
+
+  for (address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at 0x");
+      if (address < 16) Serial.print("0");
+      Serial.println(address, HEX);
+      found++;
+    }
+  }
+
+  if (found == 0) {
+    Serial.println("No I2C devices found.");
+    Serial.println("Check: power (3V3/GND) + SDA(21) + SCL(22).");
+  } else {
+    Serial.print("Done. Devices found: ");
+    Serial.println(found);
+  }
+
+  Serial.println();
+  delay(2000);
 }
